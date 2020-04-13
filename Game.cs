f4069f924b168007cb0b3755d503ec2343788001
byBlurr <dbldumont11@gmail.com>
2020-04-13 03:05:27 +0100

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,55 @@
+﻿using BEngine2D;
+using BEngine2D.Input;
+using BEngine2D.Render;
+using BEngine2D.Util;
+using System;
+using System.Drawing;
+using System.Numerics;
+
+namespace BExampleGame
+{
+    public class Game : BGame
+    {
+        BTexture2D[] Textures = new BTexture2D[200];
+
+        public Game(string title, double fps, double ups) : base(title, fps, ups)
+        {
+
+        }
+        public override void OnLoad()
+        {
+            base.OnLoad();
+            Textures[0] = BContentPipe.LoadTexture("Textures/tree.png");
+            Textures[1] = BContentPipe.LoadTexture("Textures/grass.png");
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+            // Todo: Draw World
+
+            // Draw random ass tree
+            BGraphics.Draw(Textures[0], Vector2.Zero, new Vector2(0.2f), Color.Transparent, new Vector2(10, 50));
+        }
+
+        public override void Tick(double delta)
+        {
+            base.Tick(delta);
+        }
+
+        public override void OnMouseDown(BMouseButton button, System.Numerics.Vector2 position)
+        {
+            base.OnMouseDown(button, position);
+
+            if (button.Equals(BMouseButton.Left))
+            {
+                Vector2 pos = new Vector2(position.X, position.Y);
+                pos -= new Vector2(Width, Height) / 2f;
+                pos = Camera.ToWorld(pos);
+
+                Camera.SetPosition(pos, BTweenType.QuadraticInOut, 120);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using BEngine2D.Util;
+
+namespace BExampleGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppInfo.APP_NAME = "Tree Planter";
+            AppInfo.APP_DEVELOPER = "Blurr Development";
+            AppInfo.APP_VERSION = "v0.1";
+            AppInfo.APP_RELEASE = Release.Alpha;
+            AppInfo.APP_UPDATE_DATE = "April 12 2020";
+            AppInfo.APP_WATERMARK = $"This is the {AppInfo.APP_RELEASE} version of {AppInfo.APP_NAME}. This does not represent the final product.";
+            AppInfo.DISPLAY_APP_WATERMARK = true;
+
+            Game game = new Game(AppInfo.APP_NAME, 30.0, 144.0);
+        }
+    }
+}
